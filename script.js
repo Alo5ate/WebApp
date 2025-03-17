b7edc43cda05eb77a028f3496fd278301183e791
MK <mainmail@mail.com>
2025-03-17 20:43:56 +0000

    Moderately comple categories

--- script.js
+++ script.js
@@ -4,9 +4,14 @@ const backbtn = document.querySelector(".back-btn");
 const clas1Tog = document.querySelector(".class1");
 const clas2Tog = document.querySelector(".class2");
 const clas3Tog = document.querySelector(".class3");
+const clas4Tog = document.querySelector(".class4");
 const classbackBtn = document.querySelector(".classback-btn");
 const classbackBtnTwo = document.querySelector(".classback-btn_two");
 const classbackBtnThree = document.querySelector(".classback-btn_three");
+const classbackBtnfour = document.querySelector(".classback-btn_four");
+
+const faqs = document.querySelectorAll(".faq");
+
 
 const toggleScreen = () =>{
     wrapper.classList.toggle("show-category")
@@ -20,6 +25,10 @@ const toggleScreenPage2 = () =>{
 
 const toggleScreenPage3 = () =>{
     wrapper.classList.toggle("show-page3")
+};
+
+const toggleScreenPage4 = () =>{
+    wrapper.classList.toggle("show-page4")
 };
 
 
@@ -38,3 +47,13 @@ classbackBtnTwo.addEventListener("click", toggleScreenPage2);
 
 clas3Tog.addEventListener("click", toggleScreenPage3);
 classbackBtnThree.addEventListener("click", toggleScreenPage3);
+
+clas4Tog.addEventListener("click", toggleScreenPage4);
+classbackBtnfour.addEventListener("click", toggleScreenPage4);
+
+
+faqs.forEach((faq) =>{
+    faq.addEventListener("click", () =>{
+        faq.classList.toggle("active");
+    });
+}) ;
